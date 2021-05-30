9e65b4b55e599fa39db481b338da6a22b7b2b3cd
Mikhail Stepchenko <enenlingre@gmail.com>
2021-05-30 07:16:11 +0300

    fix: redis url in env

--- src/adapters/redis-io.adapter.ts
+++ src/adapters/redis-io.adapter.ts
@@ -2,7 +2,7 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import * as redisIoAdapter from 'socket.io-redis';
 
-const redisAdapter = redisIoAdapter({ host: 'localhost', port: 6379 });
+const redisAdapter = redisIoAdapter(process.env.REDIS_URL);
 
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions): any {
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { UserController } from "./user.controller";
 import { UserSchema } from "./user.schema";
 import { UserService } from "./user.service";
 
@@ -7,6 +8,7 @@ import { UserService } from "./user.service";
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UserService],
   exports: [UserService],
+  controllers: [UserController],
 })
 
 export class UserModule {}
